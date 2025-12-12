2db7a15382e1e02097abd1cafee197a257c07607
Guilherme <krohling.gonzaga@gmail.com>
2025-12-12 14:35:56 -0300

    first commit

---
+++ taskflow/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header className="bg-blue-600 text-white p-4 flex justify-between items-center gap-4">
+            <h1 className="text-2xl font-bold text-white">TaskFlow</h1>
+            <div className="flex gap-4">
+                <button className="bg-gray-900">Tema</button>
+                <button className="bg-gray-900">Idioma</button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
